bb43f7e24258628d8d833b8085abc2128cb36667
Ekrem <103184982+ekrembardak@users.noreply.github.com>
2022-05-14 16:15:40 +0300

    Add files via upload

---
+++ usluSayi.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class usluSayi {
+    public static void main(String[] args) {
+        int n,k;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayı: ");
+        n = input.nextInt();
+        System.out.print("Üssü: ");
+        k = input.nextInt();
+        int total = 1;
+        
+        for(int i = 1; i <= k ; i++ ) {
+            total *= n;
+            
+        }
+        System.out.println("Sonuç : " + total);
+   
+}
+    
+}
