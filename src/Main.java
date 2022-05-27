09f3da55ab684596b7740b9ee8dc6e38b02b326a
Ekrem <103184982+ekrembardak@users.noreply.github.com>
2022-05-27 13:45:16 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Course mat = new Course("Matematik","12121","MAT");
-        Course fizik = new Course("Fizik","34343","FIZ");
-        Course kimya = new Course("Kimya","56565","KIM");
+        Fighter ekrem = new Fighter("Ekrem" , 15 , 100, 90, 0);
+        Fighter fikret = new Fighter("Fikret" , 10 , 95, 100, 0);
+        Match r = new Match(ekrem,fikret , 90 , 100);
+        r.run();
+    }
+    }
 
-        Teacher t1 = new Teacher("Albus Dumbledore","MAT","12121");
-        Teacher t2 = new Teacher("Severus Snape","KIM","56565");
-
-        Student s1 = new Student("Ekrem Bardak","1996",7,mat,kimya,fizik);
-        Student s2 = new Student("Fatih Bardak","2002",8,mat,kimya,fizik);
-
-        s1.addBulkExamNotes(75,40,60);
-        s1.printNote();
-
-        System.out.println(t1.getBranch());
-        System.out.println(t1.getName());
-   
-}
-    
-}
