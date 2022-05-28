85dc91fae292a922a3502cacd52240683c1bc3d2
Ekrem <103184982+ekrembardak@users.noreply.github.com>
2022-05-28 19:48:14 +0300

    Add files via upload

---
+++ letter.java
@@ -0,0 +1,25 @@
+public class letter {
+    public static void main(String[] args) {
+        String [][] letter = new String [7][4];
+         String[][] arrB=new String[7][4];
+        for(int row=0;row<arrB.length;row++){
+            for(int col=0;col<arrB[row].length;col++){
+                if(row==0 || row==3 || row==6){
+                    arrB[row][col]="*";
+                }
+                else if(col==0 || col==3){
+                    arrB[row][col]="*";
+                }
+                else{
+                    arrB[row][col]=" ";
+                }
+            }
+        }
+        for(String[] row:arrB){
+            for(String col:row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
